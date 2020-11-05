6a84fa768e7fc1589ff601211e7f81f1dba82f8c
Matthew Morris <matthewmorris09@gmail.com>
2020-11-05 06:57:53 -0600

    Added date to Notion maybe

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def addGoodReadsPercent(token, collectionURL, percent):
     cv = client.get_collection_view(collectionURL)
     row = cv.collection.add_row()
     row.title = "Test123"
-    CST = tz.gettz('America/Chicago')
-    row.date = datetime.now(CST)
+    #CST = tz.gettz('America/Chicago')
+    #now = datetime.now()
+    row.date = cv.NotionDate(datetime.now())
     percent = int(percent)
     percent = float(percent/100)
     row.percent = percent
